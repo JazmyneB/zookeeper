8deb3d762fb41f96b91d893f1d0807ed9b17a811
JazmyneB <bradleyj1@email.arizona.edu>
2021-07-12 22:24:50 -0700

    added get and server

--- server.js
+++ server.js
@@ -47,6 +47,11 @@ function filterByQuery(query, animalsArray){
     return filteredResults;
 }
 
+function findById(id, animalsArray){
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 
 
 
@@ -60,6 +65,17 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+//New ROUTE foro animals
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    //res.json(result);
+    if (result){
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
